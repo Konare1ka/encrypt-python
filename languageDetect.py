40a4734fe37ed385e9dec9ece9dea2821bf66ea6
Made in China <162896476+Konare1ka@users.noreply.github.com>
2025-09-27 21:54:47 +0500

    Delete languageDetect.py

--- languageDetect.py
+++
@@ -1,9 +0,0 @@
-#Determine language in which text is written
-def detect(text):
-    if any('а' <= char <= 'я' or char == 'ё' for char in text.lower()):
-        return 'ru'
-    elif any('a' <= char <= 'z' for char in text.lower()):
-        return 'en'
-    else: 
-        print("Language not detected")
-        return 'no'
